b44115532c4be18e086b20471e9504fb275144e7
Yuri Artemev <i@artemeff.com>
2013-07-14 13:41:28 +0400

    add expm

---
+++ package.exs
@@ -0,0 +1,8 @@
+Expm.Package.new(
+  name: "exredis",
+  description: "Redis client for Elixir",
+  version: "0.0.1",
+  keywords: ["elixir", "redis", "database"],
+  maintainers: [[name: "Yuri Artemev", email: "i@artemeff.com"]],
+  repositories: [[github: "artemeff/exredis"]]
+)
