0f7cac8defc5b73d4a891efb0824b79999593a2f
Aidan Steele <aidan.steele@glassechidna.com.au>
2014-01-13 15:22:16 +1100

    Made tests pass

--- lib/exredis/api.ex
+++ lib/exredis/api.ex
@@ -11,7 +11,8 @@ defmodule Exredis.Api.Helper do
       def unquote(cmd)(client, unquote_splicing(margs)) do
         method = String.upcase atom_to_binary unquote(cmd)
         f = unquote(fun)
-        res = Exredis.query client, [method, unquote_splicing(margs)]
+        query_args = List.flatten [method|[unquote_splicing(margs)]]
+        res = Exredis.query client, query_args
         if f, do: f.(res), else: res
       end
 
@@ -45,47 +46,52 @@ defmodule Exredis.Api do
     pid
   end
 
-  defredis :append, [:key, :value]
+  defredis :append, [:key, :value], &int_reply/1
   defredis :auth, [:password]
   defredis :bgrewriteaof, []
   defredis :bgsave, []
-  defredis :bitcount, [:key]#, :start, :end]
+  defredis :bitcount, [:key, :start, :end], &int_reply/1
+  defredis :bitcount, [:key], &int_reply/1
   defredis :bitop, [:operation, :destkey, :key]#, ...]
   defredis :blpop, [:key, :timeout]
   defredis :brpop, [:key, :timeout]
   defredis :brpoplpush, [:source, :destination, :timeout]
   defredis :dbsize, []
+  defredis :decr, [:key], &int_reply/1
+  defredis :decrby, [:key, :decrement], &int_reply/1
+  defredis :del, [:key], &int_reply/1
   defredis :discard, []
   defredis :dump, [:key]
   defredis :echo, [:message]
   defredis :exec, []
-  defredis :exists, [:key]
-  defredis :expire, [:key, :seconds]
-  defredis :expireat, [:key, :timestamp]
-  defredis :flushall, []
+  defredis :exists, [:key], &int_reply/1
+  defredis :expire, [:key, :seconds], &int_reply/1
+  defredis :expireat, [:key, :timestamp], &int_reply/1
+  defredis :flushall, [], &sts_reply/1
   defredis :flushdb, []
   defredis :get, [:key]
-  defredis :getbit, [:key, :offset]
+  defredis :getbit, [:key, :offset], &int_reply/1
   defredis :getrange, [:key, :start, :end]
   defredis :getset, [:key, :value]
-  defredis :hdel, [:key, :field]#, ...]
-  defredis :hexists, [:key, :field]
+  defredis :hdel, [:key, :field], &int_reply/1#, ...]
+  defredis :hexists, [:key, :field], &int_reply/1
   defredis :hget, [:key, :field]
   defredis :hgetall, [:key], fn x ->
     Enum.chunk(x, 2) 
       |> Enum.map(fn [a, b] -> {a, b} end) 
       |> HashDict.new
   end
-  defredis :hincrby, [:key, :field, :increment]
+  defredis :hincrby, [:key, :field, :increment], &int_reply/1
   defredis :hincrbyfloat, [:key, :field, :increment]
   defredis :hkeys, [:key]
-  defredis :hlen, [:key]
+  defredis :hlen, [:key], &int_reply/1
   defredis :hmget, [:key, :field]#, ...]
-  defredis :hset, [:key, :field, :value]
-  defredis :hsetnx, [:key, :field, :value]
+  defredis :hmset, [:key, :vals], &sts_reply/1
+  defredis :hset, [:key, :field, :value], &int_reply/1
+  defredis :hsetnx, [:key, :field, :value], &int_reply/1
   defredis :hvals, [:key]
-  defredis :incr, [:key]
-  defredis :incrby, [:key, :increment]
+  defredis :incr, [:key], &int_reply/1
+  defredis :incrby, [:key, :increment], &int_reply/1
   defredis :incrbyfloat, [:key, :increment]
   # defredis :info, []
   defredis :keys, [:pattern]
@@ -100,28 +106,28 @@ defmodule Exredis.Api do
   defredis :lrem, [:key, :count, :value]
   defredis :lset, [:key, :index, :value]
   defredis :ltrim, [:key, :start, :stop]
-  defredis :mget, [:key]#, ...]
+  defredis :mget, [:key], &sts_reply/1#, ...]
   # defredis :migrate
   defredis :monitor, []
   defredis :move, [:key, :db]
-  defredis :mset, [:key, :value]#, ...]
+  defredis :mset, [:vals], &sts_reply/1#, ...]
   defredis :msetnx, [:key, :value]#, ...]
   defredis :multi, []
   # defredis :object, []
-  defredis :persist, [:key]
-  defredis :pexpire, [:key, :milliseconds]
-  defredis :pexpireat, [:key, :milli_timestamp]
+  defredis :persist, [:key], &int_reply/1
+  defredis :pexpire, [:key, :milliseconds], &int_reply/1
+  defredis :pexpireat, [:key, :milli_timestamp], &int_reply/1
   defredis :ping, []
   defredis :psetex, [:key, :milliseconds, :value]
   defredis :psubscribe, [:pattern]#, ...]
   # defredis :pubsub, [:subcommand]
-  defredis :pttl, [:key]
-  defredis :publish, [:channel, :message]
+  defredis :pttl, [:key], &int_reply/1
+  defredis :publish, [:channel, :message], &int_reply/1
   defredis :punsubscribe, [:pattern]#, ...]
   defredis :quit, []
   defredis :randomkey, []
-  defredis :rename, [:key, :newkey]
-  defredis :renamex, [:key, :newkey]
+  defredis :rename, [:key, :newkey], &sts_reply/1
+  defredis :renamenx, [:key, :newkey], &int_reply/1
   defredis :restore, [:key, :ttl, :serialized_value]
   defredis :rpop, [:key]
   defredis :rpoplpush, [:source, :destination]
@@ -137,11 +143,11 @@ defmodule Exredis.Api do
   defredis :sdiff, [:key]#, ...]
   defredis :sdiffstore, [:destination, :key]#, ...]
   defredis :select, [:index]
-  defredis :set, [:key, :value]#, :seconds, :milli]
-  defredis :setbit, [:key, :offset, :value]
-  defredis :setex, [:key, :seconds, :value]
-  defredis :setnx, [:key, :value]
-  defredis :setrange, [:key, :offset, :value]
+  defredis :set, [:key, :value], &sts_reply/1
+  defredis :setbit, [:key, :offset, :value], &int_reply/1
+  defredis :setex, [:key, :seconds, :value], &sts_reply/1
+  defredis :setnx, [:key, :value], &int_reply/1
+  defredis :setrange, [:key, :offset, :value], &int_reply/1
   # defredis :shutdown, [:nosave, :save]
   defredis :sinter, [:key]#, ...]
   defredis :sinterstore, [:destination, :key]#, ...]
@@ -154,13 +160,13 @@ defmodule Exredis.Api do
   defredis :spop, [:key]
   defredis :srandmember, [:key]#, :count]
   defredis :srem, [:key, :member]#, ...]
-  defredis :strlen, [:key]
+  defredis :strlen, [:key], &int_reply/1
   defredis :subscribe, [:channel]#, ...]
   defredis :sunion, [:key]#, ...]
   defredis :sunionstore, [:destination, :key]#, ...]
   defredis :sync, []
   defredis :time, []
-  defredis :ttl, [:key]
+  defredis :ttl, [:key], &int_reply/1
   defredis :type, [:key]
   defredis :unsubscribe, [:channel]#, ...]
   defredis :unwatch, []
