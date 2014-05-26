af5c6fbcfc25be93bea522533e3c826d46412d68
ty <lastcanal@gmail.com>
2014-05-26 11:22:31 -0400

    add default keys on script test setup and cleanup non-existing sha test

--- test/script_test.exs
+++ test/script_test.exs
@@ -13,13 +13,15 @@ defmodule ScriptTest do
 
   @lua_script "return {KEYS,ARGV}"
   @lua_script_sha "667538fc7bc1737be485fe688c8f39e7ddc79782"
+  @lua_script_sha_non_existing "9c609a7ccf8e2ef9459f7f88c414c951e3d174d9"
 
   setup do
     client = E.start
 
     # clean up database and set test value
     client |> E.query ["FLUSHALL"]
-    client |> E.query ["SET", "key", "value"]
+    client |> E.query ["SET", "key1", "value1"]
+    client |> E.query ["SET", "key2", "value2"]
 
     { :ok, [c: client] }
   end
@@ -34,7 +36,7 @@ defmodule ScriptTest do
   test "script exists", c do
     assert (c[:c] |> R.script_load(@lua_script)) == @lua_script_sha
     assert (c[:c] |> R.script_exists(@lua_script_sha)) == [1]
-    assert (c[:c] |> R.script_exists([@lua_script_sha, "abcd"])) == [1, 0]
+    assert (c[:c] |> R.script_exists([@lua_script_sha, @lua_script_sha_non_existing])) == [1, 0]
   end
 
   test "script flush", c do
