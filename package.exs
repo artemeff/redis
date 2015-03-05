715d05d4cdf130b516b8c5ad07e32b58d9a75c3d
Yuri Artemev <i@artemeff.com>
2015-03-05 12:22:34 +0300

    cleanup

--- package.exs
+++
@@ -1,8 +0,0 @@
-Expm.Package.new(
-  name: "exredis",
-  description: "Redis client for Elixir",
-  version: "0.0.1",
-  keywords: ["elixir", "redis", "database"],
-  maintainers: [[name: "Yuri Artemev", email: "i@artemeff.com"]],
-  repositories: [[github: "artemeff/exredis"]]
-)
