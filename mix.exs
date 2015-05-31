919044a3885aea3e9ca525867e02b4a21a32ca8f
Yuri Artemev <i@artemeff.com>
2015-05-31 23:18:30 +0300

    update exredis to 0.1.3

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Exredis.Mixfile do
 
   def project do
     [ app: :exredis,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.0.0",
       name: "exredis",
       source_url: "https://github.com/artemeff/exredis",
