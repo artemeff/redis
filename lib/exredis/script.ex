c915f6024e32b9538a40f75aafb2ed97d1d4ec8c
Courtney Couch <courtney.couch@gmail.com>
2015-09-27 20:59:38 -0700

    loading the script if it's missing, then calling with evalsha

--- lib/exredis/script.ex
+++ lib/exredis/script.ex
@@ -11,11 +11,21 @@ defmodule Exredis.Script do
       def unquote(name)(client, keys \\ [], argv \\ []) do
         query_args = [length(keys)] ++ keys ++ argv
         case Exredis.query client, ["EVALSHA", unquote(script_sha)] ++ query_args do
-          <<"NOSCRIPT", _ :: binary>> ->
-            Exredis.query client, ["EVAL", unquote(script)] ++ query_args
+          <<"NOSCRIPT", _ :: binary>> -> 
+            load_script client, unquote(script)
+            unquote(name)(client, keys, argv)
           reply -> reply
         end
       end
+    end
+  end
+
+  def load_script(client, script) do
+    case Exredis.query client, ["SCRIPT", "LOAD", script] do
+      <<"ERR", error :: binary>> ->
+        throw error
+      reply -> 
+        reply
     end
   end
 end
